edae2065a0832f70a0b9756f59f02282ee4eefbc
Marek Dudek <marek.dudek@gmail.com>
2019-12-08 21:57:23 +0100

    Java 11 application that builds to native image with Graal VM

---
+++ src/main/java/org/example/java11withgraal/Java11WithGraalMain.java
@@ -0,0 +1,10 @@
+package org.example.java11withgraal;
+
+import static java.lang.System.out;
+
+public final class Java11WithGraalMain {
+
+    public static void main(String[] args) {
+        out.println("This is Java 11 with Graal VM application");
+    }
+}
